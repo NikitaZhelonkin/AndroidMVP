35a2ec08d2c793c55a4b7041025ee798a599c5fb
Nikita Zhelonkin <niks.mbox@gmail.com>
2018-11-02 16:46:54 +0300

    Make PresenterProvider public

--- library/src/main/java/ru/nikitazhelonkin/mvp/android/PresenterProvider.java
+++ library/src/main/java/ru/nikitazhelonkin/mvp/android/PresenterProvider.java
@@ -7,7 +7,7 @@ import android.support.v4.app.FragmentActivity;
 
 import ru.nikitazhelonkin.mvp.MvpPresenter;
 
-class PresenterProvider {
+public class PresenterProvider {
 
     public interface Factory<T extends MvpPresenter> {
         T create();
